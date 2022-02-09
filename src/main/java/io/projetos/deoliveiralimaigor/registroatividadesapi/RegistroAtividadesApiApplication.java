75f70231e5e0e7ff984dd97b02e50b21062fea3f
igor <deoliveiralimaigor@gmail.com>
2022-02-09 18:08:49 -0300

    frist commit

---
+++ src/main/java/io/projetos/deoliveiralimaigor/registroatividadesapi/RegistroAtividadesApiApplication.java
@@ -0,0 +1,13 @@
+package io.projetos.deoliveiralimaigor.registroatividadesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroAtividadesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroAtividadesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/projetos/deoliveiralimaigor/registroatividadesapi/RegistroAtividadesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.projetos.deoliveiralimaigor.registroatividadesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroAtividadesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
