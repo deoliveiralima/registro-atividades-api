4cf96dd71200941d76e71eb5194f2163b0f8e3e9
Igor Lima <deoliveiralimaigor@gmail.com>
2022-03-04 14:16:23 -0300

    commit

--- src/main/java/io/projetos/deoliveiralimaigor/registroatividadesapi/config/SecurityConfiguration.java
+++ src/main/java/io/projetos/deoliveiralimaigor/registroatividadesapi/config/SecurityConfiguration.java
@@ -50,7 +50,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-			
+			.antMatchers(HttpMethod.POST, "/*").permitAll()
+			.antMatchers(HttpMethod.POST, "/*/").permitAll()
         	.antMatchers(HttpMethod.POST, "/auth*").permitAll()
 			.antMatchers(HttpMethod.POST, "/usuario/*").permitAll()
 			.antMatchers(HttpMethod.POST, "/*").permitAll()
